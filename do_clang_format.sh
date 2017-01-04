b720297558fa6e20d0ef5afa2fec05d16a4c0485
Paul Dreik <rdfind@pauldreik.se>
2017-01-04 11:44:40 +0100

    add clang format file made with clang-format-3.5 -style=GNU -dump-config >.clang-format on debian jessie

---
+++ do_clang_format.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+#executes clang format
+#
+#
+# copyright 2017 Paul Dreik
+# Distributed under GPL v 2.0 or later, at your option.
+# See LICENSE for further details.
+
+#find clang format (usually named clang-format-3.x or clang-format, who knows)
+CLANGFORMAT=$(find /usr/local/bin /usr/bin -executable -name "clang-format*" |grep -v -- -diff |sort -g |tail -n1)
+
+if [ ! -x "$CLANGFORMAT" ] ; then
+    echo failed finding clangformat
+    exit 1
+else
+    echo found clang format: $CLANGFORMAT
+fi
+
+find . -maxdepth 1 -type f \( -name "*.h" -o -name "*.cpp" -o -name "*.cc" -o -name "*.hh" \) -print0 | \
+    xargs -0 -n1 $CLANGFORMAT -i
