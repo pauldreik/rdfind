030defa865ca0dbc35d3538110a29ba6c4123f37
Paul Dreik <rdfind@pauldreik.se>
2018-10-15 09:01:31 +0200

    #7 add test for selecting checksum

---
+++ testcases/checksum_options.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# Test that selection of checksum works as expected.
+
+
+set -e
+. "$(dirname "$0")/common_funcs.sh"
+
+
+
+
+
+for checksumtype in md5 sha1 sha256; do
+  reset_teststate
+  dbgecho "trying checksum $checksumtype"
+  echo checksumtest >a
+  echo checksumtest >b
+  $rdfind  -checksum $checksumtype -deleteduplicates true a b
+  [ -e a ]
+  [ ! -e b ]
+done
+
+dbgecho "all is good in this test!"
