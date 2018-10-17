b18f0569e85b226f684fd29fa71220c8b7c79287
Paul Dreik <rdfind@pauldreik.se>
2018-10-17 09:34:38 +0200

    suppress error message when looking for libc++

--- do_quality_checks.sh
+++ do_quality_checks.sh
@@ -158,8 +158,8 @@ fi
 #make a test program to make sure it works.
 echo "#include <iostream>
 int main() { std::cout<<\"libc++ works!\";}" >x.cpp
-if ! $latestclang -std=c++11 -stdlib=libc++ -lc++abi x.cpp && ./a.out ; then
-  echo "$latestclang could not compile with libc++"
+if ! $latestclang -std=c++11 -stdlib=libc++ -lc++abi x.cpp >/dev/null 2>&1 && [ -x ./a.out ] && ./a.out ; then
+  echo "$latestclang could not compile with libc++ - perhaps uninstalled."
   return 0
 fi
 #echo using $latestclang with libc++
