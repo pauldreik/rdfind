b2f34eb29333c2236a63749052813d8c50d729b6
Robert Marklund <robbelibobban@gmail.com>
2025-01-13 16:36:04 +0100

    add buffersize argument
    add the argument to change buffer size of checksum calculation,
    may increase speed on some checksum algorithms and disk types.
    
    Signed-off-by: Robert Marklund <robbelibobban@gmail.com>

--- Rdutil.hh
+++ Rdutil.hh
@@ -88,9 +88,9 @@ public:
   // if there is trouble with too much disk reading, sleeping for nsecsleep
   // nanoseconds can be made between each file.
   int fillwithbytes(enum Fileinfo::readtobuffermode type,
-                    enum Fileinfo::readtobuffermode lasttype =
-                      Fileinfo::readtobuffermode::NOT_DEFINED,
-                    long nsecsleep = 0);
+                    enum Fileinfo::readtobuffermode lasttype,
+                    long nsecsleep,
+                    std::size_t buffersize);
 
   /// make symlinks of duplicates.
   std::size_t makesymlinks(bool dryrun) const;
