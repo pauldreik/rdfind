ec413aa404c641c6753e7cecb797967a607b457d
Robert Marklund <robbelibobban@gmail.com>
2025-01-23 16:26:16 +0100

    fix shellcheck in all files except test cases
    enable shellcheck tests in do_shellcheck.sh
    
    Signed-off-by: Robert Marklund <robbelibobban@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,11 +8,11 @@
 #bail out on error
 set -e
 
-me=$(basename $0)
+me="$(basename "$0")"
 
 for prog in aclocal autoheader automake autoconf make; do
    if ! which $prog >/dev/null 2>&1 ; then
-      echo $me: please install $prog
+      echo "$me: please install $prog"
       exit 1
    fi
 done
--- do_shellcheck.sh
+++ do_shellcheck.sh
@@ -7,9 +7,7 @@ me=$(basename "$0")
 
 echo "$me: run shellcheck on shellscripts"
 (
-    # use this when all issues are fixed
-    # git ls-files | grep -v "^testcases" | grep -E ".sh$" | xargs shellcheck
-    shellcheck do_shellcheck.sh do_yamllint.sh
+    git ls-files | grep -v "^testcases" | grep -E ".sh$" | xargs shellcheck
 )
 
 echo "$me: run shellcheck on testcases"
